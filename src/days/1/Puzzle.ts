ed9058b93adfb8fc00391080ca6c4ef990772ecc
Minqi Chen <cnchenminqi@gmail.com>
2024-12-01 18:23:24 -0800

    Day 1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,15 +1,43 @@
+import * as _ from 'lodash';
+
 const first = (input: string) => {
-  console.log(input);
-  return 'solution 1';
+  const inputRows = parseList(input);
+
+  const [first, second] = _.zip(...inputRows);
+
+  first.sort();
+  second.sort();
+
+  return _.zip(first, second)
+    .map(([a, b]) => Math.abs(b - a))
+    .reduce((prev, curr) => prev + curr, 0);
 };
 
-const expectedFirstSolution = 'solution 1';
+const expectedFirstSolution = 11;
 
 const second = (input: string) => {
-  console.log(input);
-  return 'solution 2';
+  const inputRows = parseList(input);
+
+  const [first, second] = _.zip(...inputRows);
+
+  const appearances = second.reduce(
+    (acc, val) => {
+      acc[val] = (acc[val] ?? 0) + 1;
+      return acc;
+    },
+    {} as Record<number, number>
+  );
+
+  const output = first.reduce((acc, val) => {
+    return acc + val * (appearances[val] ?? 0);
+  }, 0);
+  return output;
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 31;
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
+
+const parseList = (input: string) => {
+  return input.split('\n').map((line) => line.trim().split(/\s+/).map(Number));
+};
